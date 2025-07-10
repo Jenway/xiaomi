5c7d0f1e920765102cee62dda9f13eef7f755a88
jenway <jenwayhere@gmail.com>
2025-07-10 21:21:36 +0800

    fix(rpath): fix hardcode shared library  path

--- 05/05.sh
+++ 05/05.sh
@@ -1,1 +1,8 @@
 mkdir -p build && cmake -Bbuild . && cmake --build build && ./build/shared_hello
+echo
+echo "检查 RUNPATH 设置"
+readelf -d build/shared_hello | grep -i 'PATH'
+
+echo
+echo "查看链接库"
+ldd build/shared_hello | grep libhello
